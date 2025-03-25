40ee2dec52d11cfbe28781b04ddb9a307477569d
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-03-25 15:44:16 +0300

    Added the Header and Footer elements to the site using Bootstrap

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,33 +3,22 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
-export default [
-	{ ignores: ["dist"] },
-	{
-		files: ["**/*.{js,jsx}"],
-		languageOptions: {
-			ecmaVersion: 2020,
-			globals: globals.browser,
-			parserOptions: {
-				ecmaVersion: "latest",
-				ecmaFeatures: { jsx: true },
-				sourceType: "module",
-			},
-		},
-		plugins: {
-			"react-hooks": reactHooks,
-			"react-refresh": reactRefresh,
-		},
-		rules: {
-			// turn this one off
-			"react/prop-types": "off",
-			...js.configs.recommended.rules,
-			...reactHooks.configs.recommended.rules,
-			"no-unused-vars": "warn",
-			"react-refresh/only-export-components": [
-				"warn",
-				{ allowConstantExport: true },
-			],
-		},
+module.exports = {
+	env: { browser: true, es2020: true },
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
+		"plugin:react-hooks/recommended",
+	],
+	parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+	settings: { react: { version: "18.2" } },
+	plugins: ["react-refresh"],
+	rules: {
+		// turn this one off
+		"react/prop-types": "off",
+		// change these errors to warnings
+		"react-refresh/only-export-components": "warn",
+		"no-unused-vars": "warn",
 	},
-];
+};
