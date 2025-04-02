c442662a0f17ee4eddebd57bb91aa48479e0306c
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-02 21:07:02 +0300

    Removed Vite-EsLint and using default eslint.config.cjs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// import the eslint plugin
-import eslintPlugin from "vite-plugin-eslint";
-
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		eslintPlugin({
-			// setup the plugin
-			cache: false,
-			include: ["./src/**/*.js", "./src/**/*.jsx"],
-			exclude: [],
-		}),
-	],
+	plugins: [react()],
 	server: {
 		proxy: {
 			"/api": "http://localhost:5000",
