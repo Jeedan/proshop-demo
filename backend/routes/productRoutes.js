32bfb180a9671be9b8ce635bde325a7dc05f97c4
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-05 16:36:20 +0300

    added a productController.js to clean up our productRoutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,13 @@
 import express from "express";
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
+import {
+	getProducts,
+	getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get(
-	"/",
-	asyncHandler(async (req, res) => {
-		// pass in empty object to grab all products
-		const products = await Product.find({});
-		res.json(products);
-	})
-);
+router.route("/").get(getProducts);
 
-router.get(
-	"/:id",
-	asyncHandler(async (req, res) => {
-		// get id from url
-		const product = await Product.findById(req.params.id);
-		if (product) {
-			return res.json(product);
-		}
-		// if no product found return a 404 error
-		res.status(404);
-		throw new Error("Resource not found");
-	})
-);
+router.route("/:id").get(getProductById);
 
 export default router;
