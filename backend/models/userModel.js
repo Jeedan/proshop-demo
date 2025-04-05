0b04e4a611487cf9feda515baff4d6fa25ca1d33
Jeedan <Jeedan.Cuneyt@gmail.com>
2025-04-05 14:42:22 +0300

    added productModel and userModel Mongoose Schemas

---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		isAdmin: {
+			type: Boolean,
+			default: false,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// create an instance of the userschema
+const User = mongoose.model("User", userSchema);
+
+export default User;
